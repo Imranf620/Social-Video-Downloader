7e677b3d2cf42e60923a3fe12ecfa039acbc74aa
imranf620 <imranf620@gmail.com>
2024-10-23 00:52:12 +0500

    insta

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React from "react";
+
+import Link from "next/link";
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+import { ThemeToggle } from "./theme-toggle";
+import { MobileNav } from "./mobile-nav";
+
+export function Navbar() {
+  return (
+    <header className="h-fit w-full">
+      <nav
+        className={cn(
+          "z-50",
+          "flex h-[3.5rem] px-4",
+          "w-full items-center border-b bg-accent/20"
+        )}
+      >
+        <div className="hidden select-none text-xl font-extrabold uppercase md:block">
+          Downloader
+        </div>
+        <MobileNav className="md:hidden" />
+        <div className="ml-auto flex items-center gap-4">
+          <div className="hidden items-center gap-4 text-lg sm:gap-8 sm:pr-8 md:flex">
+            <Link href="/" className="hover:underline">
+              Home
+            </Link>
+            <Link
+              target="_blank"
+              // href="https://github.com/riad-azz/instagram-video-downloader"
+              href="/"
+              className="hover:underline"
+            >
+              Github
+            </Link>{" "}
+            <Link
+              target="_blank"
+              href="/"
+              // href="https://github.com/riad-azz"
+              className="hover:underline"
+            >
+              Creator
+            </Link>
+          </div>
+          <ThemeToggle />
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ src/lib/constants.ts
@@ -0,0 +1,63 @@
+// type NavLink = {
+//   name: string;
+//   href: string;
+// };
+
+// export const siteLinks: NavLink[] = [
+//   {
+//     name: "Home",
+//     href: "/",
+//   },
+//   {
+//     name: "Terms of Service",
+//     href: "/terms-of-service",
+//   },
+//   {
+//     name: "Privacy Policy",
+//     href: "/privacy-policy",
+//   },
+// ];
+
+// export const externalLinks: NavLink[] = [
+//   {
+//     name: "Github Repository",
+//     href: "https://github.com/riad-azz/instagram-video-downloader",
+//   },
+//   {
+//     name: "Creator",
+//     href: "https://www.instagram.com/riadazz/",
+//   },
+// ];
+
+
+
+type NavLink = {
+  name: string;
+  href: string;
+};
+
+export const siteLinks: NavLink[] = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Terms of Service",
+    href: "/",
+  },
+  {
+    name: "Privacy Policy",
+    href: "/",
+  },
+];
+
+export const externalLinks: NavLink[] = [
+  {
+    name: "Github Repository",
+    href: "/",
+  },
+  {
+    name: "Creator",
+    href: "/",
+  },
+];
